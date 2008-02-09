bc70b6d170cfa079d9ddba0cc979192089ff0a1e
zyor.taelon <zyor.taelon@cf400a95-ad45-0410-a29f-a50bf25af4c7>
2008-02-09 01:41:06 +0000

    Unnessesary comments removed.

--- trunk/src/main/java/com/betterbe/eveapi/starbase/ListResponse.java
+++ trunk/src/main/java/com/betterbe/eveapi/starbase/ListResponse.java
@@ -19,17 +19,17 @@ public class ListResponse extends ApiResponse {
 		return starbases;
 	}
 
+	private void addTypeId(int itemID, Map<Integer, Integer> typeIds) {
+		int amount = 0;
+		if (typeIds.containsKey(itemID)) {
+			amount = typeIds.get(itemID);
+			typeIds.remove(itemID);
+		}
+		typeIds.put(itemID, amount + 1);
+	}
+
 	@Override
 	public String toString() {
-		// Caldari Control Tower Medium = 1
-		// Amarr Control Tower Small = 2
-		// Caldari Control Tower = 2
-		// Domination Control Tower = 4
-		// Gallente Control Tower = 4
-		// Domination Control Tower Medium = 7
-		// Minmatar Control Tower Small = 10
-		// Minmatar Control Tower = 21
-
 		String result = "Current time: " + getCurrentTime() + "\nCached Untill: " + getCachedUntil() + "\nWe currently have " + starbases.size()
 				+ " starbases in space\n\n";
 		Map<Integer, Integer> typeIds = new HashMap<Integer, Integer>();
@@ -42,14 +42,5 @@ public class ListResponse extends ApiResponse {
 			result += entry.getKey() + " = " + entry.getValue() + "\n";
 		}
 		return result;
-	}
-
-	private void addTypeId(int itemID, Map<Integer, Integer> typeIds) {
-		int amount = 0;
-		if (typeIds.containsKey(itemID)) {
-			amount = typeIds.get(itemID);
-			typeIds.remove(itemID);
-		}
-		typeIds.put(itemID, amount + 1);
 	}
 }
