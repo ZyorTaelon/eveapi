98d0d9e0f7ece69606faec914cdf85dc39449729
zyor.taelon <zyor.taelon@cf400a95-ad45-0410-a29f-a50bf25af4c7>
2008-02-11 23:53:34 +0000

    Setup basic structure for map and general util requests.

--- trunk/src/main/java/com/beimin/eveapi/utils/stationlist/Parser.java
+++ trunk/src/main/java/com/beimin/eveapi/utils/stationlist/Parser.java
@@ -1,11 +1,20 @@
 package com.beimin.eveapi.utils.stationlist;
 
+import org.apache.commons.digester.Digester;
+
 import com.beimin.eveapi.AbstractApiParser;
 
 public class Parser extends AbstractApiParser<Response> {
 
 	public Parser() {
 		super(Response.class);
+	}
+
+	@Override
+	protected Digester getDigester() {
+		Digester digester = super.getDigester();
+		// create digester here.
+		return digester;
 	}
 
 	public static Parser getInstance() {
