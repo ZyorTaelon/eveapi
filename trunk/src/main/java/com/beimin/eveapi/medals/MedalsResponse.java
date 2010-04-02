3a026b770087759e28fcb38aa49f892d47fd9379
zyor.taelon <zyor.taelon@cf400a95-ad45-0410-a29f-a50bf25af4c7>
2010-04-02 20:00:27 +0000

    MemberMedals parser added

--- trunk/src/main/java/com/beimin/eveapi/medals/MedalsResponse.java
+++ trunk/src/main/java/com/beimin/eveapi/medals/MedalsResponse.java
@@ -6,13 +6,13 @@ import java.util.List;
 import com.beimin.eveapi.ApiResponse;
 
 public class MedalsResponse extends ApiResponse {
-	private final List<Medal> medal = new ArrayList<Medal>();
+	private final List<Medal> medals = new ArrayList<Medal>();
 
 	public void addMedal(Medal entry) {
-		medal.add(entry);
+		medals.add(entry);
 	}
 
 	public List<Medal> getMedals() {
-		return medal;
+		return medals;
 	}
 }
---
+++ trunk/src/test/java/com/beimin/eveapi/member/medals/ParserTest.java
@@ -0,0 +1,33 @@
+package com.beimin.eveapi.member.medals;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+public class ParserTest {
+
+	@Test
+	public void testMemberTrackingParser() throws IOException, SAXException {
+		MemberMedalsParser parser = MemberMedalsParser.getInstance();
+		InputStream input = ParserTest.class.getResourceAsStream("/MemberMedals.xml");
+		MemberMedalsResponse response = parser.getResponse(input);
+		assertNotNull(response);
+		List<MemberMedal> memberMedals = response.getMemberMedals();
+		assertNotNull(memberMedals);
+		assertEquals(45, memberMedals.size());
+		MemberMedal memberMedal = memberMedals.iterator().next();
+		assertEquals(1745, memberMedal.getMedalID());
+		assertEquals(264288979L, memberMedal.getCharacterID());
+		assertEquals("Ooy late night op", memberMedal.getReason());
+		assertTrue("Should have been public", memberMedal.isPublic());
+		assertEquals(817217271L, memberMedal.getIssuerID());
+		assertEquals("2008-11-12 07:39:28", memberMedal.getIssued());
+	}
+}
