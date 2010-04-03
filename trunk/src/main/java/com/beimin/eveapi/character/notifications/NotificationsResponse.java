d8b8db5e7b1d82b9c1da3c96c69217b9c7d92681
zyor.taelon <zyor.taelon@cf400a95-ad45-0410-a29f-a50bf25af4c7>
2010-04-03 16:58:15 +0000

    make collection private final

--- trunk/src/main/java/com/beimin/eveapi/character/notifications/NotificationsResponse.java
+++ trunk/src/main/java/com/beimin/eveapi/character/notifications/NotificationsResponse.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import com.beimin.eveapi.ApiResponse;
 
 public class NotificationsResponse extends ApiResponse {
-	Set<ApiNotification> mails = new HashSet<ApiNotification>();
+	private final Set<ApiNotification> mails = new HashSet<ApiNotification>();
 
 	public void addApiMail(ApiNotification member) {
 		mails.add(member);
