98be03433fd885139c48da1162e3655cb888402a
zyor.taelon@gmail.com <zyor.taelon@gmail.com@cf400a95-ad45-0410-a29f-a50bf25af4c7>
2010-11-12 21:38:24 +0000

    Issue 12: made the CharacterLookupParser.getCharacterList() return an ApiResponse object inline with the rest of the parsers.

--- trunk/src/main/java/com/beimin/eveapi/eve/character/CharacterLookupParser.java
+++ trunk/src/main/java/com/beimin/eveapi/eve/character/CharacterLookupParser.java
@@ -1,14 +1,14 @@
 package com.beimin.eveapi.eve.character;
 
 import java.io.IOException;
-import java.util.Collection;
 
 import org.apache.commons.digester.Digester;
 import org.xml.sax.SAXException;
 
 import com.beimin.eveapi.AbstractApiParser;
 
-public class CharacterLookupParser extends AbstractApiParser<CharacterLookupResponse> {
+public class CharacterLookupParser extends
+		AbstractApiParser<CharacterLookupResponse> {
 	protected static final String CHARACTER_NAME_URL = "/eve/CharacterName.xml.aspx";
 	protected static final String CHARACTER_ID_URL = "/eve/CharacterID.xml.aspx";
 	private final String paramName;
@@ -18,30 +18,33 @@ public class CharacterLookupParser extends AbstractApiParser<CharacterLookupResp
 		this.paramName = paramName;
 	}
 
-	public Collection<ApiCharacterLookup> getCharacterList(String... arguments) throws IOException, SAXException {
+	public CharacterLookupResponse getCharacterList(String... arguments)
+			throws IOException, SAXException {
 		String paramValue = "";
 		for (int i = 0; i < arguments.length; i++) {
-			paramValue+=arguments[i];
-			if(i < arguments.length-1)
-				paramValue+=",";
+			paramValue += arguments[i];
+			if (i < arguments.length - 1)
+				paramValue += ",";
 		}
-		return getResponse(paramName, paramValue.replaceAll(" ", "%20")).getCharacterLookups();
+		return getResponse(paramName, paramValue.replaceAll(" ", "%20"));
 	}
 
-	public Collection<ApiCharacterLookup> getCharacterList(long... arguments) throws IOException, SAXException {
+	public CharacterLookupResponse getCharacterList(long... arguments)
+			throws IOException, SAXException {
 		String paramValue = "";
 		for (int i = 0; i < arguments.length; i++) {
-			paramValue+=arguments[i];
-			if(i < arguments.length-1)
-				paramValue+=",";
+			paramValue += arguments[i];
+			if (i < arguments.length - 1)
+				paramValue += ",";
 		}
-		return getResponse(paramName, paramValue).getCharacterLookups();
+		return getResponse(paramName, paramValue);
 	}
 
 	@Override
 	protected Digester getDigester() {
 		Digester digester = super.getDigester();
-		digester.addObjectCreate("eveapi/result/rowset/row", ApiCharacterLookup.class);
+		digester.addObjectCreate("eveapi/result/rowset/row",
+				ApiCharacterLookup.class);
 		digester.addSetProperties("eveapi/result/rowset/row");
 		digester.addSetNext("eveapi/result/rowset/row", "addCharacterLookup");
 		return digester;
