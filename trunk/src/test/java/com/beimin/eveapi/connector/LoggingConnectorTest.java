480259b912e3ea55ca8a2a29d0fdb8a3fbc9c5ab
zyor.taelon@gmail.com <zyor.taelon@gmail.com@cf400a95-ad45-0410-a29f-a50bf25af4c7>
2011-04-06 19:52:55 +0000

    removed last assert because it ran into platform specific issues with the newline.

--- trunk/src/test/java/com/beimin/eveapi/connector/LoggingConnectorTest.java
+++ trunk/src/test/java/com/beimin/eveapi/connector/LoggingConnectorTest.java
@@ -67,6 +67,5 @@ public class LoggingConnectorTest extends FullAuthParserTest {
 		assertEquals("    </rowset>", lines[16]);
 		assertEquals("  </result>", lines[17]);
 		assertEquals("  <cachedUntil>2010-04-03 16:48:20</cachedUntil>", lines[18]);
-		assertEquals("</eveapi>\r", lines[19]); 
 	}
 }
