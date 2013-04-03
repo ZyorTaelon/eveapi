98454a35e3a767bd7b0b7a3b895e9d36edb42be5
zyor.taelon@gmail.com <zyor.taelon@gmail.com@cf400a95-ad45-0410-a29f-a50bf25af4c7>
2013-04-03 20:54:35 +0000

    revert release (again)

--- eveapi/tags/eveapi-5.1.2/src/main/java/com/beimin/eveapi/character/medals/CharacterMedalsResponse.java
+++
@@ -1,7 +0,0 @@
-package com.beimin.eveapi.character.medals;
-
-import com.beimin.eveapi.core.ApiListResponse;
-
-public class CharacterMedalsResponse extends ApiListResponse<CharacterMedal> {
-	private static final long serialVersionUID = 1L;
-}
--- eveapi/tags/eveapi-5.1.2/src/main/java/com/beimin/eveapi/eve/reftypes/RefTypesResponse.java
+++
@@ -1,7 +0,0 @@
-package com.beimin.eveapi.eve.reftypes;
-
-import com.beimin.eveapi.core.ApiListResponse;
-
-public class RefTypesResponse extends ApiListResponse<ApiRefType> {
-	private static final long serialVersionUID = 1L;
-}
