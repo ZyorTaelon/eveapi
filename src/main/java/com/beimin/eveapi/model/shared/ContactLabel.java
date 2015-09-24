eeab0ec04584881347ee5f7c785bd42ae3e1c349
Steve Lewis <stevelle@gmail.com>
2015-09-23 22:28:26 -0700

    Add ContactLabel model & attribs to Contact model
    ContactLabel is available to be parsed in responses for personal,
    corporate, and alliance contacts. A model object is added to capture
    this data and exposed through the ContactListParser.
    
    Simiarly two attributes which were available in all contacts have
    been added to the model and the values parsed from the APIresponse.
    
    Fixes: #52
    Fixes: #53

---
+++ src/main/java/com/beimin/eveapi/model/shared/ContactLabel.java
@@ -0,0 +1,25 @@
+package com.beimin.eveapi.model.shared;
+
+/**
+ *
+ */
+public class ContactLabel {
+    private int labelID;
+    private String name;
+
+    public int getLabelID() {
+        return labelID;
+    }
+
+    public void setLabelID(int labelID) {
+        this.labelID = labelID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
