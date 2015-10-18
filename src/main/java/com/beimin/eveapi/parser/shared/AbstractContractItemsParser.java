f6f64272c5b98756489f2ef5dd3ee611f943bec8
Jens Oberender <j.obi@troja.net>
2015-10-18 22:10:31 +0200

    Cleaned java doc of AbstractContractItemsParser, clean up pom file and removed assembly xml as we will deploy to maven central soon.

--- src/main/java/com/beimin/eveapi/parser/shared/AbstractContractItemsParser.java
+++ src/main/java/com/beimin/eveapi/parser/shared/AbstractContractItemsParser.java
@@ -11,21 +11,18 @@ import com.beimin.eveapi.parser.ApiPage;
 import com.beimin.eveapi.parser.ApiPath;
 import com.beimin.eveapi.response.shared.ContractItemsResponse;
 
+/**
+ * There are two methods, one for API keys with only a single character and one for multi-character API keys.
+ */
 public abstract class AbstractContractItemsParser extends AbstractListParser<ContractItemsHandler, ContractItemsResponse, ContractItem> {
 	protected AbstractContractItemsParser(ApiPath path) {
 		super(ContractItemsResponse.class, 2, path, ApiPage.CONTRACT_ITEMS, ContractItemsHandler.class);
 	}
 
-	/**
-	 * Use this when your API Key is connected to a single character
-	 */
 	public ContractItemsResponse getResponse(ApiAuthorization auth, long contractID) throws ApiException {
 		return super.getResponse(auth, "contractID", Long.toString(contractID));
 	}
 
-	/**
-	 * Use this when your API Key is connected to multiple characters
-	 */
 	public ContractItemsResponse getResponse(ApiAuthorization auth, long characterID, long contractID) throws ApiException {
 		Map<String, String> extraParams = new HashMap<String, String>();
 		extraParams.put("characterID", Long.toString(characterID));
