4bb14241d406aaff4ba2a3fa53233dc1819c6312
Jens Oberender <j.obi@troja.net>
2015-10-18 15:46:03 +0200

    Fixed deprecated warning.

--- src/test/java/com/beimin/eveapi/eve/reftypes/RefTypeEnumGenerator.java
+++ src/test/java/com/beimin/eveapi/eve/reftypes/RefTypeEnumGenerator.java
@@ -20,7 +20,7 @@ public class RefTypeEnumGenerator {
 		RefTypesParser parser = new RefTypesParser();
 		RefTypesResponse response = parser.getResponse();
 		Collection<RefType> refTypes = response.getAll();
-		Configuration cfg = new Configuration();
+		Configuration cfg = new Configuration(Configuration.VERSION_2_3_23);
 		Template tpl = cfg.getTemplate("src/test/resources/refType.ftl");
 		FileWriter output = new FileWriter("src/test/java/com/beimin/eveapi/shared/wallet/RefType.java");
 		HashMap<String, Object> model = new HashMap<String, Object>();
