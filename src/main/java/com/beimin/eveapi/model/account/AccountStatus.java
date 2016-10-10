64d619543f70a5ecb250c9ce3468327d166a7950
Jens Oberender <j.obi@troja.net>
2016-10-10 22:45:50 +0200

    Removed AccountStatus.userID

--- src/main/java/com/beimin/eveapi/model/account/AccountStatus.java
+++ src/main/java/com/beimin/eveapi/model/account/AccountStatus.java
@@ -3,19 +3,10 @@ package com.beimin.eveapi.model.account;
 import java.util.Date;
 
 public class AccountStatus {
-    private int userID; // >271604</userID>
     private Date paidUntil; // >2011-03-13 18:40:00</paidUntil>
     private Date createDate; // >2004-07-22 23:54:00</createDate>
     private int logonCount; // >5603</logonCount>
     private int logonMinutes; // >504903</logonMinutes>
-
-    public int getUserID() {
-        return userID;
-    }
-
-    public void setUserID(final int userID) {
-        this.userID = userID;
-    }
 
     public Date getPaidUntil() {
         return paidUntil;
--- src/test/java/com/beimin/eveapi/account/accountstatus/AccountStatusParserOnlineTest.java
+++ src/test/java/com/beimin/eveapi/account/accountstatus/AccountStatusParserOnlineTest.java
@@ -5,11 +5,10 @@ import org.junit.Test;
 import com.beimin.eveapi.AbstractOnlineTest;
 import com.beimin.eveapi.parser.account.AccountStatusParser;
 import com.beimin.eveapi.response.account.AccountStatusResponse;
-import org.junit.Ignore;
 
 public class AccountStatusParserOnlineTest extends AbstractOnlineTest {
 
-    @Test @Ignore("userID no longer returned by the API")
+    @Test
     public void getResponse() throws Exception {
         final AccountStatusParser accountStatusParser = new AccountStatusParser();
         final AccountStatusResponse response = accountStatusParser.getResponse(getAccount());
