14dfd0312183143a83f273b717090b93e4246514
GoldenGnu <GoldenGnu@users.noreply.github.com>
2016-10-20 14:16:59 +0200

    Added missing fields and removed dead fields from CharacterSheetResponse.

--- src/main/java/com/beimin/eveapi/model/pilot/Role.java
+++ src/main/java/com/beimin/eveapi/model/pilot/Role.java
@@ -19,4 +19,29 @@ public class Role {
     public void setRoleName(final String roleName) {
         this.roleName = roleName;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 13 * hash + (int) (this.roleID ^ (this.roleID >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Role other = (Role) obj;
+        if (this.roleID != other.roleID) {
+            return false;
+        }
+        return true;
+    }
 }
