6f3029457b06013cbb80ecb57558d1adf161ec72
GoldenGnu <GoldenGnu@users.noreply.github.com>
2016-10-25 15:00:25 +0200

    Changed ignore in AllianceListParserOnlineTest

--- src/test/java/com/beimin/eveapi/eve/alliancelist/AllianceListParserOnlineTest.java
+++ src/test/java/com/beimin/eveapi/eve/alliancelist/AllianceListParserOnlineTest.java
@@ -3,16 +3,13 @@ package com.beimin.eveapi.eve.alliancelist;
 import org.junit.Test;
 
 import com.beimin.eveapi.AbstractOnlineTest;
-import com.beimin.eveapi.TestControl;
 import com.beimin.eveapi.parser.eve.AllianceListParser;
 import com.beimin.eveapi.response.eve.AllianceListResponse;
-import static org.junit.Assume.assumeTrue;
 
 public class AllianceListParserOnlineTest extends AbstractOnlineTest {
 
     @Test
     public void getResponse() throws Exception {
-        assumeTrue("No data returned by the API", TestControl.runNoData());
         final AllianceListParser parser = new AllianceListParser();
         prepareParser(parser);
 
