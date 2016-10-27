25674d31a55672c419326ddbc3e477398c97159d
GoldenGnu <GoldenGnu@users.noreply.github.com>
2016-10-27 16:27:35 +0200

    Added missing rowsets to MedalsParser

--- src/main/java/com/beimin/eveapi/response/pilot/MedalsResponse.java
+++ src/main/java/com/beimin/eveapi/response/pilot/MedalsResponse.java
@@ -2,6 +2,27 @@ package com.beimin.eveapi.response.pilot;
 
 import com.beimin.eveapi.model.pilot.Medal;
 import com.beimin.eveapi.response.ApiListResponse;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MedalsResponse extends ApiListResponse<Medal> {
+
+    private final Set<Medal> currentCorporation = new HashSet<>();
+    private final Set<Medal> otherCorporations = new HashSet<>();
+
+    public void addCurrentCorporation(Medal medal) {
+        currentCorporation.add(medal);
+    }
+
+    public Set<Medal> getCurrentCorporation() {
+        return currentCorporation;
+    }
+
+    public void addOtherCorporations(Medal medal) {
+        otherCorporations.add(medal);
+    }
+
+    public Set<Medal> getOtherCorporations() {
+        return otherCorporations;
+    }
 }
