ca2dcb726b314920028e2dadf3f58dcf91ae8cdd
GoldenGnu <GoldenGnu@users.noreply.github.com>
2016-10-27 14:17:08 +0200

    Fixed ShareholdersParserOnlineTest

--- src/test/java/com/beimin/eveapi/corporation/shareholders/ShareholdersParserOnlineTest.java
+++ src/test/java/com/beimin/eveapi/corporation/shareholders/ShareholdersParserOnlineTest.java
@@ -3,6 +3,7 @@ package com.beimin.eveapi.corporation.shareholders;
 import org.junit.Test;
 
 import com.beimin.eveapi.AbstractOnlineTest;
+import com.beimin.eveapi.model.corporation.Shareholder;
 import com.beimin.eveapi.parser.corporation.ShareholdersParser;
 import com.beimin.eveapi.response.corporation.ShareholdersResponse;
 
@@ -11,6 +12,9 @@ public class ShareholdersParserOnlineTest extends AbstractOnlineTest {
     @Test
     public void getResponse() throws Exception {
         ignoreClassField(ShareholdersResponse.class, "items");
+         //No characters shareholders in test API Key
+        ignoreClassField(Shareholder.class, "shareholderCorporationID");
+        ignoreClassField(Shareholder.class, "shareholderCorporationName");
         allowNull("getShareholderCorporationName");
         allowNull("getShareholderCorporationID");
         final ShareholdersParser parser = new ShareholdersParser();
