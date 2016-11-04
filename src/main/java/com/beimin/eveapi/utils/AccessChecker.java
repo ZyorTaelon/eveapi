848d5ce07e7703308c4f79b466b3ff6d32f50b02
Jens Oberender <j.obi@troja.net>
2016-11-04 21:52:29 +0100

    Flattened CallListResponse.

--- src/main/java/com/beimin/eveapi/utils/AccessChecker.java
+++ src/main/java/com/beimin/eveapi/utils/AccessChecker.java
@@ -4,11 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.beimin.eveapi.model.calllist.Call;
-import com.beimin.eveapi.model.calllist.CallList;
 import com.beimin.eveapi.response.account.ApiKeyInfoResponse;
+import com.beimin.eveapi.response.calllist.CallListResponse;
 
 public class AccessChecker {
-    public static Set<Call> getCalls(final ApiKeyInfoResponse apiKeyInfo, final CallList callList) {
+    public static Set<Call> getCalls(final ApiKeyInfoResponse apiKeyInfo, final CallListResponse callList) {
         final Set<Call> result = new HashSet<Call>();
         for (final Call call : callList.getCalls()) {
             final long cam = call.getAccessMask();
